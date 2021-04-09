cbdb6309c9de7e10b4ddab02cc0aed561b43dee0
Olninyo <ollyskillman@gmail.com>
2021-04-09 17:12:26 +0100

    environment lighting update working!

--- Assets/Scripts/EnvironmentLighting.cs
+++ Assets/Scripts/EnvironmentLighting.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UpdateEnvironmentLighting : MonoBehaviour
+public class EnvironmentLighting : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        DynamicGI.UpdateEnvironment();
     }
 
     // Update is called once per frame
